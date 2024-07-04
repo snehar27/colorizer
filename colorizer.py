ddc7f4220d84194b5a72ab0020b953de72e84af1
Sneha Rajaraman <sr934@cornell.edu>
2024-07-04 11:55:52 -0400

    created Argparser

--- colorizer.py
+++ colorizer.py
@@ -2,9 +2,15 @@ import numpy as np
 import argparse
 import cv2
 import os
-# import PROTOTXT from "models/colorization_deploy_v1.prototxt";
+# import colo from "models/colorization_deploy_v1.prototxt";
 
 DIR = r"/Users/sneha_rajaraman/Desktop/colorize"
 PROTOTXT = os.path.join(DIR, r"models/colorization_deploy_v1.prototxt")
 POINTS = os.path.join(DIR, r"models/pts_in_hull.npy")
 MODEL = os.path.join(DIR, r"models/colorization_release_v2.caffemodel")
+
+# Argparser: Provides the path for the input image
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--image", type=str, required=True,
+                    help="input path for black & white image") # allows to pass in different images by providing their path
+args = vars(parser.parse_args())
