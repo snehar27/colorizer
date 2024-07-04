e8f07b7927c4d089cc71655ff1a9b483373cac23
Sneha Rajaraman <sr934@cornell.edu>
2024-07-04 11:49:37 -0400

    loaded models

--- colorizer.py
+++ colorizer.py
@@ -2,3 +2,9 @@ import numpy as np
 import argparse
 import cv2
 import os
+# import PROTOTXT from "models/colorization_deploy_v1.prototxt";
+
+DIR = r"/Users/sneha_rajaraman/Desktop/colorize"
+PROTOTXT = os.path.join(DIR, r"models/colorization_deploy_v1.prototxt")
+POINTS = os.path.join(DIR, r"models/pts_in_hull.npy")
+MODEL = os.path.join(DIR, r"models/colorization_release_v2.caffemodel")
